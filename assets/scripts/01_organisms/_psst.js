40a38583092c6acb5cfb63687e30a4796094c42d
sandrina-p <sandrina.pereira@zaask.com>
2016-10-21 11:20:19 +0100

    change psst star scrolling

--- assets/scripts/01_organisms/_psst.js
+++ assets/scripts/01_organisms/_psst.js
@@ -1,15 +1,8 @@
 var Psst = function() {
     var hasTouch = Modernizr.touchevents;
-    function changePsst(context) {
-        if (!hasTouch) {
-            var $jsPsst = $('.js-psst');
-            $jsPsst.find('.psst-title').html(chatContent.behaviour[context][0]);
-            $jsPsst.find('.psst-parag').html(chatContent.behaviour[context][1]);
-            $jsPsst.find('button[name="true"]').html(chatContent.behaviour[context][2]);
-            $jsPsst.find('button[name="false"]').html(chatContent.behaviour[context][3]);
-        }
-    }
 
+
+    var imgg = ``;
     (function onTouchScreen() {
         if (hasTouch) {
             var $jsPsst = $('.js-psst');
@@ -19,6 +12,17 @@ var Psst = function() {
             $jsPsst.find('button[name="false"]').remove();
         }
     })();
+
+
+    function changePsst(context) {
+        if (!hasTouch) {
+            var $jsPsst = $('.js-psst');
+            $jsPsst.find('.psst-title').html(chatContent.behaviour[context][0]);
+            $jsPsst.find('.psst-parag').html(chatContent.behaviour[context][1]);
+            $jsPsst.find('button[name="true"]').html(chatContent.behaviour[context][2]);
+            $jsPsst.find('button[name="false"]').html(chatContent.behaviour[context][3]);
+        }
+    }
     return {
         changePsst
     }
