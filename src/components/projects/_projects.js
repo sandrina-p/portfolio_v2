d7e9507f30d06470f4ee050d8d1104eae4448f9f
Sandrina Pereira <sandrin4pereir4@gmail.com>
2017-04-15 20:07:33 +0100

    a11y :: cv projects with aria-labelledby

--- src/components/projects/_projects.js
+++ src/components/projects/_projects.js
@@ -601,7 +601,7 @@ var cvProjects = function(){
 
         hightlightProject(ArrProj[projI]);
 
-        $cvProjects.on('mouseenter', function(){
+        $cvProjects.on('mouseenter, focus', function(){
             removeActive();
             setNewSub($(this).data('sub'));
             setProjLoop(false);
