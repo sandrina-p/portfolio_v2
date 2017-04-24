488520dbb350a9b3c762e4dfe8c01cd952e96b70
Sandrina Pereira <sandrin4pereir4@gmail.com>
2017-04-24 15:57:32 +0100

    add preload to fonts links

--- views/head.php
+++ views/head.php
@@ -1,8 +1,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300,400,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,700" rel="stylesheet">
+    <link rel="preload" href="https://fonts.googleapis.com/css?family=Roboto+Mono:300,400,700" rel="stylesheet">
+    <link rel="preload" href="https://fonts.googleapis.com/css?family=Ubuntu:400,700" rel="stylesheet">
 
     <title>Sandrina Pereira - front-end developer | UX / Interaction Design</title>
 
