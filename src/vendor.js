ba8e3236b378f2fa72bb93f823ce19a919019351
Sandrina Pereira <sandrin4pereir4@gmail.com>
2017-05-20 11:59:14 +0100

    remove typedjs

--- src/vendor.js
+++ src/vendor.js
@@ -1,7 +1,3 @@
 // -- during offline development -- =inc//lude vendor/jquery/jquery-3.2.1.min.js
 
-// =incl--ude vendor/modernizr/modernizr-flexbox.js
-// =incl--ude vendor/modernizr/modernizr-touch_event.js
-
-// =include vendor/typedjs-master/dist/typed.min.js
 // =include vendor/baffle/baffle.min.js
