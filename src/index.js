51524d1ef3596f19dce1b54b347da86a52701a48
Sandrina Pereira <sandrina.pereira@farfetch.com>
2017-12-15 19:23:14 +0000

    chore: Run build

--- src/index.js
+++ src/index.js
@@ -15,21 +15,3 @@
 // =include components/url/_hashs.js
 
 // =include components/tracking/_ga.js
-
-
-// --------- ¯\_(ツ)_/¯ ---------  //
-
-console.warn('%cPsst... yes, you!', 'color: #00e8e8; size: 15px; font-size: 1.5rem; font-weight: 600');
-console.warn(
-    `
-%cDon't judge Sandrina's technical skills only based on this code.
-I was built during 2016 and a lot she's been learning since those times.
-Module approach, Unit Testing (TDD), Type Checkers, Linters ...
-All those tools and methods to have a readable and solid code.
-
-Still, I'm very accessible and that's something most of the developers (and designers) forget about!
-
-Thank you and let's the debug begin, shall we? ʘ‿ʘ
-    `,
-    'font-size: 0.8rem; line-height: 1.5'
-);
